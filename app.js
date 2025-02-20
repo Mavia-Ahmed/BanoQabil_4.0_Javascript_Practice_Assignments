c60ffe282c65608e24e369d95a248a2d1b24bc3b
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-02-20 06:12:11 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,176 @@
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+15
+
+
+18
+19
+20
+
+
+23
+*/
+/* Q2 */
+
+ /*
+document.write("Hello World");
+*/
+/* Q25*/
+
+/*
+let number = prompt("Enter a number: ");
+let answer = 0;
+for (let i = 1; i <= 10; i++) {
+    answer = number * i;
+    document.write(`${number} x ${i} = ${answer} <br>`);
+}
+*/
+
+/* Q24 */
+
+/* 
+alert("Cost of 1 movie ticket is: 600 pkr");
+let ticket = 600;
+let total = ticket *5;
+document.write(`Total cost of 5 tickets is: ${total}pkr`);
+*/
+
+/* Q23 */
+
+/*a */
+var num;
+
+/*b */
+document.write("Value after variable declaration is: " + num + "<br>");
+
+/*c */
+num = 5;
+
+/*d */
+document.write("Initial value: " + num + "<br>");
+
+/*e */
+num++;
+
+/*f */
+document.write("Value after increment is: " + num + "<br>");
+
+/*g */
+num += 7;
+
+/*h */
+document.write("Value after addition is: " + num + "<br>");
+
+/*i */
+num--;
+
+/*j */
+document.write("Value after decrement is: " + num + "<br>");
+
+/*k */
+var remainder = num % 3;
+
+// l. Output remainder
+document.write("The remainder is: " + remainder + "<br>");
+
+
+
+/* Q21 & Q22 */
+
+/*
+let num1 = prompt("Enter first number: ");
+let num2 = prompt("Enter second number: ");
+let option = prompt("Enter\n1 for addition\n2 for subtraction\n3 for multiplication\n4 for division\n5 for modulus: ");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+option = parseInt(option);
+if (option == 1){
+    let answer = alert(num1 + num2);
+}else if (option == 2){
+    let answer = alert(num1 - num2);
+}else if (option == 3){
+    let answer = alert(num1 * num2);
+}else if(option == 4){
+    let answer = alert(num1 / num2);
+}else{
+    alert("Invalid Option")
+}
+*/
+
+
+/* Q1 & Q17 */
+
+/*
+alert("Welcome to our clothing store");
+var customerName = prompt("Please enter your name: ");
+var productTitle = prompt ("Please enter the product you want to buy: ");
+var Quantity = prompt("Please enter the quantity you want to buy: ");
+document.write(`${customerName} ordered ${Quantity} ${productTitle} on our clothing store`);
+*/
+
+
+/* Q14 & Q16*/
+
+/*
+var birthYear = 2005;
+var age = 18;
+alert(`My age is ${age} and my birth year is ${birthYear}`);
+*/
+
+
+/*Q11 */
+
+/*
+var email = ("neewwx666@gmail.com");
+alert(`My email address is ${email}`);
+*/
+
+/* Q10 */
+
+/*
+var name = "Mavia Khan";
+var course = "Web Developemnt 2";
+var days = "Tuesday & Thursday";
+var timings = "8:00 Pm to 10:00 pm";
+var institute = "Idara Noor-e-Haq";
+alert(`Name: ${name}\nCourse: ${course}\nDays: ${days}\nTimings: ${timings}\nInstitute: ${institute}`);
+*/
+
+/* Q9 */
+
+/*
+var titledMessage = "Hello World";
+alert(titledMessage);
+*/
+
+
+/* Q8 */
+
+/*
+var firstName = "Mavia";
+var lastName = "Khan";
+var myName = firstName + " " + lastName;
+alert(myName)
+*/
+
+
+
+
+
+
+
+
+
+
